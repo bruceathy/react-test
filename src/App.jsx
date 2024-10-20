489d7c86ef51a8272f6a3b9c61b94c0e68f1f8d8
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-10-20 03:23:57 -0400

    header folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import Header from "./components/header";
-import Card from "./components/card";
+import Header from "./components/Header/Header";
+import Card from "./components/Card";
 
 function App() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
-import reactLogo from "../assets/react.svg";
+import reactLogo from "../../assets/react.svg";
 import viteLogo from "/vite.svg";
+import "./Header.css";
+
 function Header() {
   return (
     <div>
